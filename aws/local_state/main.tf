7277da485e9f63281ac73079fedbcfdd2138e65b
gow20 <gow2097@gmail.com>
2025-03-12 00:02:09 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -2,23 +2,22 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 4.67"
     }
   }
 
   required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region  = "us-west-2"
+provider "aws" {                
+  region  = "ap-south-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+resource "aws_instance" "Jenkins" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Terraform_Demo"
+    Name = "Jenkins_Terraform"
   }
 }
 
